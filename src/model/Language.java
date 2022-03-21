b1cbf7b2104872c3c34c9caf8a0af4713516c1bb
Alexandre Luís <alexandreluisrigotti@gmail.com>
2022-03-21 02:42:34 -0300

    Add methods for Language class

--- src/model/Language.java
+++ src/model/Language.java
@@ -21,4 +21,24 @@ public class Language
         this.languageId = languageId;
         this.languageName = languageName;
     }
+
+    public int getLanguageId()
+    {
+        return languageId;
+    }
+
+    public void setLanguageId(int languageId)
+    {
+        this.languageId = languageId;
+    }
+
+    public String getLanguageName()
+    {
+        return languageName;
+    }
+
+    public void setLanguageName(String languageName)
+    {
+        this.languageName = languageName;
+    }
 }
